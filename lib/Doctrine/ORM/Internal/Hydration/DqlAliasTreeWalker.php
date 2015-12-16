1630fa873f8ce79e8f63af4ed5225446e714ff29
Miroslav Sustek <sustmidown@centrum.cz>
2015-12-16 14:39:32 +0100

    hotfix for DDC-1960
    http://www.doctrine-project.org/jira/browse/DDC-1960

---
+++ lib/Doctrine/ORM/Internal/Hydration/DqlAliasTreeWalker.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Doctrine\ORM\Internal\Hydration;
+
+class DqlAliasTreeWalker {
+
+    private function getDqlAliasLevel($dqlAlias, $parentAliasMap, &$levelsCache) {
+        if (!isset($levelsCache[$dqlAlias])) {
+            if (!isset($parentAliasMap[$dqlAlias])) {
+                return 0;
+            } else {
+                return $this->getDqlAliasLevel($parentAliasMap[$dqlAlias], $parentAliasMap, $levelsCache) + 1;
+            }
+        }
+
+        return $levelsCache[$dqlAlias];
+    }
+
+    public function getDqlAliasesOrderedByLevel($dqlAliases, $parentAliasMap) {
+        $dqlAliasesByLevel = [];
+        $levelsCache = [];
+
+        foreach ($dqlAliases as $dqlAlias) {
+            $level = $this->getDqlAliasLevel($dqlAlias, $parentAliasMap, $levelsCache);
+            $dqlAliasesByLevel[$level][] = $dqlAlias;
+        }
+
+        $dqlAliasesOrderedByLevel = [];
+        for ($level = 0; isset($dqlAliasesByLevel[$level]); $level++) {
+            foreach ($dqlAliasesByLevel[$level] as $dqlAliases) {
+                $dqlAliasesOrderedByLevel[] = $dqlAliases;
+            }
+        }
+
+        return $dqlAliasesOrderedByLevel;
+    }
+
+}
